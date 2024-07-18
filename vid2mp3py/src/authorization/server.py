45bf6204d346e9741f9780ccbc872a38fc95c067
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-07-18 20:01:41 +0300

    Added JWT creation and validation

--- vid2mp3py/src/authorization/server.py
+++ vid2mp3py/src/authorization/server.py
@@ -1,5 +1,5 @@
 import os
-import jwt # Json web token
+import jwt # JSON web token
 import datetime
 from flask import Flask, request
 from flask_mysqldb import MySQL
@@ -26,7 +26,7 @@ def login():
 	cursor = my_sql.connection.cursor()
 	# Query the DB for the password of the user
 	res = cursor.execute(
-		f'SELECT password FROM user WHERE email={auth.username}'
+		f'SELECT email, password FROM user WHERE email={auth.username}'
 	)
 	# Res is an array
 	print(res)
@@ -40,4 +40,41 @@ def login():
 		if auth.username != email or auth.password != password:
 			return 'Credentials were invalid', 401
 		else:
-			return "Success", 200
+			return create_jwt(auth.username, os.environ.get('JWT_SECRET'), True)
+	else:
+		return "Credentials were invalid", 401
+
+
+def create_jwt(username, jwt_secret, is_admin):
+	return jwt.encode(
+		{
+			'username': username,
+			'exp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(days=1),
+			'iat': datetime.datetime.now(datetime.UTC),
+			'admin': is_admin
+		},
+		jwt_secret,
+		algorithm='HS256'
+	)
+
+
+@server.route('/validate', methods=['POST'])
+def validate():
+	encoded_jwt = request.headers['Authorization']
+	if not encoded_jwt:
+		return 'Credentials were invalid', 401
+
+	# Bearer <token>
+	encoded_jwt = encoded_jwt.split(' ')[1]
+	try:
+		decoded = jwt.decode(
+			encoded_jwt, os.environ.get('JWT_SECRET'), algorithms='HS256'
+		)
+	except:
+		return "Not authorized", 403
+
+	return decoded, 200
+
+
+if __name__ == '__main__':
+	server.run(host='0.0.0.0', port=5000)
