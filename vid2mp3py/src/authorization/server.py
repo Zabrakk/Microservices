472dee310b8cc98843e0288936edae0f5cfeb996
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-07-22 18:41:29 +0300

    Convert MYSQL_PORT to int

--- vid2mp3py/src/authorization/server.py
+++ vid2mp3py/src/authorization/server.py
@@ -9,7 +9,7 @@ my_sql = MySQL(server)
 
 server.config['MYSQL_HOST'] = 	  os.environ.get('MYSQL_HOST')
 server.config['MYSQL_DB'] = 	  os.environ.get('MYSQL_DB')
-server.config['MYSQL_PORT'] = 	  os.environ.get('MYSQL_PORT')
+server.config['MYSQL_PORT'] = 	  int(os.environ.get('MYSQL_PORT'))
 server.config['MYSQL_USER'] = 	  os.environ.get('MYSQL_USER')
 server.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
 
