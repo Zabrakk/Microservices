d87ed5dd4c62adcce6202d0f050fc397d9cd09e8
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-07-29 20:53:58 +0300

    Connect to MongoDB instance running in the cluster

--- vid2mp3py/src/gateway/server.py
+++ vid2mp3py/src/gateway/server.py
@@ -10,7 +10,7 @@ from auth_svc import access
 from storage import util
 
 server = Flask(__name__)
-server.config['MONGO_URI'] = 'mongodb://host.minikube.internal:27017/videos'
+server.config['MONGO_URI'] = f'mongodb://{os.getenv("MONGODB_HOST")}:{os.getenv("MONGODB_PORT")}/videos'
 
 mongo = PyMongo(server)
 fs = gridfs.GridFS(mongo.db)
