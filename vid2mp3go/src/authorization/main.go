d982b6828aee7d96f9eead5116c7a7b6282279ff
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-08-13 16:53:53 +0300

    Added SendMethodNotAllowed()

--- vid2mp3go/src/authorization/main.go
+++ vid2mp3go/src/authorization/main.go
@@ -9,25 +9,44 @@ import (
 // TODO: Get port from environment variable
 const portNum string = "8080"
 
+// Used to create JWT tokens for users.
 func CreateJWT() {
 
 }
 
+// This method is used to send a HTTP response with status code 405.
+// Use it when receiving a request with an unallowed HTTP method.
+func SendMethodNotAllowed(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	fmt.Fprintf(w, "The method is not allowed for the requested URL.")
+}
+
+// TODO
 func Login(w http.ResponseWriter, r *http.Request) {
 	log.Println("Login request received with method", r.Method)
 	if r.Method != "POST" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintf(w, "The method is not allowed for the requested URL.")
+		SendMethodNotAllowed(w)
+	} else {
+		fmt.Fprintf(w, "Logged in")
 	}
-	fmt.Fprintf(w, "Logged in")
 }
 
+// TODO
 func Register(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method != "POST" {
+		SendMethodNotAllowed(w)
+	} else {
+		fmt.Fprintf(w, "Registered")
+	}
 }
 
+// TODO
 func Validate(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method != "POST" {
+		SendMethodNotAllowed(w)
+	} else {
+		fmt.Fprintf(w, "Validated")
+	}
 }
 
 func main() {
