135d6d415991c5d25972c89e67c4e3f2b1584cd0
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-08-13 16:46:48 +0300

    Added start points for all functions

--- vid2mp3go/src/authorization/main.go
+++ vid2mp3go/src/authorization/main.go
@@ -9,6 +9,10 @@ import (
 // TODO: Get port from environment variable
 const portNum string = "8080"
 
+func CreateJWT() {
+
+}
+
 func Login(w http.ResponseWriter, r *http.Request) {
 	log.Println("Login request received with method", r.Method)
 	if r.Method != "POST" {
@@ -18,11 +22,21 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Logged in")
 }
 
+func Register(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Validate(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	log.Println("Authorization service starting...")
 
 	// Register handler functions to routes
 	http.HandleFunc("/login", Login)
+	http.HandleFunc("/register", Register)
+	http.HandleFunc("/validate", Validate)
 
 	log.Println("Authorization service running on port", portNum)
 
