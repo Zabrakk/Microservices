47802023fae921dea644d4476a1847df205bb797
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-08-16 18:19:24 +0300

    Added tests for SendStatus functions

---
+++ vid2mp3go/src/authorization/send_status/status_test.go
@@ -0,0 +1,54 @@
+package sendstatus
+
+import (
+	"net/http"
+	"testing"
+)
+
+type MockResponseWriter struct {
+	Code int
+}
+
+func (w *MockResponseWriter) Header() http.Header {
+	return http.Header{}
+}
+
+func (w *MockResponseWriter) Write(data []byte) (int, error) {
+	return 1, nil
+}
+
+func (w *MockResponseWriter) WriteHeader(statusCode int) {
+	w.Code = statusCode
+}
+
+func TestBadRequest(t *testing.T) {
+	w := MockResponseWriter{}
+	BadRequest(&w)
+	if w.Code != 400 {
+		t.Fatal("Status code was incorrect", w.Code)
+	}
+}
+
+func TestInvalidCredentials(t *testing.T) {
+	w := MockResponseWriter{}
+	InvalidCredentials(&w)
+	if w.Code != 401 {
+		t.Fatal("Status code was incorrect", w.Code)
+	}
+}
+
+func TestMethodNowAllowed(t *testing.T) {
+	w := MockResponseWriter{}
+	MethodNotAllowed(&w)
+	if w.Code != 405 {
+		t.Fatal("Status code was incorrect", w.Code)
+	}
+}
+
+func TestInternalServerError(t *testing.T) {
+	w := MockResponseWriter{}
+	InternalServerError(&w)
+	if w.Code != 500 {
+		t.Fatal("Status code was incorrect", w.Code)
+	}
+}
