096a72c5c7607e209cab07b0101e9ec8fb329044
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-08-16 18:19:47 +0300

    Moved SendStatus functions to own file

--- vid2mp3go/src/authorization/main.go
+++ vid2mp3go/src/authorization/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	SendStatus "microservices/authorization/send_status"
 	"net/http"
 	"os"
 	"time"
@@ -39,51 +40,16 @@ func (c MySQLConf) GetDataSourceName() (dataSourceName string) {
 	return  c.User + ":" + c.Password + "@tcp(" + c.Host + ":" + c.Port + ")/" + c.DB
 }
 
-// This function is used to send a HTTP response with status code 405.
-// Use it when receiving a request with an unallowed HTTP method.
-func SendMethodNotAllowed(w http.ResponseWriter) {
-	w.WriteHeader(http.StatusMethodNotAllowed)
-	fmt.Fprintf(w, "The method is not allowed for the requested URL.")
-}
-
-// This function is used to send a HTTP response with status code 401.
-// Use it when receiving a request with invalid credentials.
-// Writes a given message to the log.
-func LogAndSendInvalidCredentials(msg string, w http.ResponseWriter) {
-	log.Println(msg)
-	w.WriteHeader(http.StatusUnauthorized)
-	fmt.Fprintf(w, "Credentials were invalid")
-}
-
-// This function is used to send a HTTP response with status code 500.
-// Use it when something unexpected occurs.
-// Writes a given message to the log.
-func LogAndSendInternalServerError(msg string, err error, w http.ResponseWriter) {
-	log.Println(msg)
-	log.Println(err.Error())
-	w.WriteHeader(http.StatusInternalServerError)
-	fmt.Fprintf(w, "Internal server error")
-}
-
-// This function is used to send a HTTP response with status code 400.
-// Use it, e.g. when required headers are missing from a request.
-// The msg parameter will be the text sent back to the user.
-func SendBadRequest(msg string, w http.ResponseWriter) {
-	log.Println(msg)
-	w.WriteHeader(http.StatusBadRequest)
-	fmt.Fprintf(w, "%s", msg)
-}
-
 // Gets the BasicAuth credentials present in a given http.Request.
 // If there are no credentials present, an error is returned.
 // If everything is ok, the username and password are returned.
 func GetBasicAuth(w http.ResponseWriter, r *http.Request) (username string, password string, ok bool) {
 	username, password, ok = r.BasicAuth(); if !ok {
-		LogAndSendInvalidCredentials("BasicAuth() was not ok", w)
+		SendStatus.InvalidCredentials(w)
 		return "", "", ok
 	}
 	if len(username) == 0 || len(password) == 0 {
-		LogAndSendInvalidCredentials("Username or password was empty", w)
+		SendStatus.InvalidCredentials(w)
 		return "", "", false
 	}
 	return username, password, ok
@@ -111,56 +77,60 @@ func CreateJWT(username string) (tokenString string, err error) {
 func Login(w http.ResponseWriter, r *http.Request) {
 	log.Println("Login request received with method", r.Method)
 	if r.Method != "POST" {
-		SendMethodNotAllowed(w)
+		SendStatus.MethodNotAllowed(w)
 		return
 	}
 	username, password, ok := GetBasicAuth(w, r); if !ok { return }
 	log.Printf("Log in request received for user %s with password %s\n", username, password)
 	rows, err := db.Query(`SELECT email, password FROM user WHERE email=?`, username)
 	if err != nil {
-		LogAndSendInternalServerError("Error occured while trying to fetch user from DB", err, w)
+		log.Printf("Error occured while trying to fetch user from DB:\n%s", err.Error())
+		SendStatus.InternalServerError(w)
 		return
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var r_user, r_password string
 		if err := rows.Scan(&r_user, &r_password); err != nil {
-			LogAndSendInternalServerError("Error occured while trying to fetch user from DB", err, w)
+			log.Printf("Error occured while trying to fetch user from DB:\n%s", err.Error())
+			SendStatus.InternalServerError(w)
 			return
 		}
 		log.Printf("Read %s %s from db\n", r_user, r_password)
 
 		if username != r_user || password != r_password {
-			LogAndSendInvalidCredentials("Invalid credentials", w)
+			SendStatus.InvalidCredentials(w)
 			return
 		} else {
 			tokenString, err := CreateJWT(r_user)
 			if err != nil {
-				LogAndSendInternalServerError("Error occured while trying tocreate JWT", err, w)
+				log.Printf("Error occured while trying to create JWT:\n%s", err.Error())
+				SendStatus.InternalServerError(w)
 				return
 			}
 			fmt.Fprintf(w, "%s", tokenString)
 			return
 		}
 	}
-	LogAndSendInvalidCredentials("Invalid credentials", w)
+	SendStatus.InvalidCredentials(w)
 }
 
 // TODO
 func Register(w http.ResponseWriter, r *http.Request) {
 	log.Println("Register request received with method", r.Method)
 	if r.Method != "POST" {
-		SendMethodNotAllowed(w)
+		SendStatus.MethodNotAllowed(w)
 		return
 	}
 	username, password := r.Header.Get("Username"), r.Header.Get("Password")
 	if username == "" || password == "" {
-		SendBadRequest("Username or Password were missing", w)
+		SendStatus.BadRequest(w)
 		return
 	}
 	result, err := db.Exec("INSTERT INTO user (email, password) VALUES (?, ?)", username, password)
 	if err != nil {
-		LogAndSendInternalServerError("Something went wrong trying to rgister user to DB", err, w)
+		log.Printf("Something went wrong trying to rgister user to DB:\n%s", err.Error())
+		SendStatus.InternalServerError(w)
 		return
 	}
 	log.Println(result.LastInsertId())
@@ -170,7 +140,7 @@ func Register(w http.ResponseWriter, r *http.Request) {
 // TODO
 func Validate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		SendMethodNotAllowed(w)
+		SendStatus.MethodNotAllowed(w)
 	}
 	fmt.Fprintf(w, "Validated")
 }
