59c58e99f955e0294edd2e9c17469087853ed7ae
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-08-19 18:59:32 +0300

    Initial work on the code

--- vid2mp3go/src/converter/main.go
+++ vid2mp3go/src/converter/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	// Connect to MongoDB
+	uri := fmt.Sprintf("mongodb://%s:%s", os.Getenv("MONGODB_HOST"), os.Getenv("MONGODB_PORT"))
+	//client
+	_, err := mongo.Connect(context.TODO(), options.Client().
+					ApplyURI(uri))
+	// mongodb+srv://<user>:<Password>@<host>:<port>
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+}
