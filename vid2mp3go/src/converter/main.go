9c9a0394ef274b55485efa8ab43b8b0b7306e262
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-08-20 19:14:32 +0300

    Added RabbitMQ consuming

--- vid2mp3go/src/converter/main.go
+++ vid2mp3go/src/converter/main.go
@@ -7,10 +7,13 @@ import (
 	"log"
 	"os"
 
+	amqp "github.com/rabbitmq/amqp091-go"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/gridfs"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+// TODO: func failOnError
 
 func GetMongoUri() (mongoUri string, err error) {
 	host, port := os.Getenv("MONGODB_HOST"), os.Getenv("MONGODB_PORT")
@@ -31,9 +34,7 @@ func main() {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	// mongodb+srv://<user>:<Password>@<host>:<port>
 
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	if err != nil { log.Fatal(err.Error()) }
 	log.Println("Connected to mongoDB")
 
 
@@ -50,5 +51,44 @@ func main() {
 	}
 	log.Println("Created GridFS buckets")
 
+	connection, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672")
+	if err != nil { log.Fatal(err.Error()) }
+	defer connection.Close()
+	log.Println("Connected to RabbitMQ")
 
+	channel, err := connection.Channel()
+	if err != nil { log.Fatal(err.Error()) }
+	defer channel.Close()
+	log.Printf("RabbitMQ channel opened")
+
+	queue, err := channel.QueueDeclare(
+		os.Getenv("VIDEO_QUEUE"),	// Name
+		true,						// Durable
+		false,						// Delete when unused
+		false,						// Exclusive
+		false,						// No-wait
+		nil,						// Args
+	)
+	if err != nil { log.Fatal(err.Error()) }
+
+	msgs, err := channel.Consume(
+		queue.Name, // Queue
+		"",     	// Consumer
+		true,   	// Auto-ack, TODO Change to false
+		false,  	// Exclusive
+		false,  	// No-local
+		false,  	// No-wait
+		nil,    	// Args
+	)
+	if err != nil { log.Fatal(err.Error()) }
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+	log.Println("Waiting for messages")
+	<-forever
 }
