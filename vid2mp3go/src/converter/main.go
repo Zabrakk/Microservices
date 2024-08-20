9cfe4cbd4433f6b8fd2a52855b2896dead43a4d6
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-08-20 17:47:35 +0300

    Added GetMongUri() + MongoDB / GridFS connections

--- vid2mp3go/src/converter/main.go
+++ vid2mp3go/src/converter/main.go
@@ -2,26 +2,53 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/gridfs"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+func GetMongoUri() (mongoUri string, err error) {
+	host, port := os.Getenv("MONGODB_HOST"), os.Getenv("MONGODB_PORT")
+	if host == "" || port == "" {
+		return "", errors.New("mongodb env variables were not set")
+	}
+	return fmt.Sprintf("mongodb://%s:%s", host, port), nil
+}
+
 func main() {
-	log.Println("Converter starting")
+	log.Println("Converter service starting...")
 	// Connect to MongoDB
 
-	uri := fmt.Sprintf("mongodb://%s:%s", os.Getenv("MONGODB_HOST"), os.Getenv("MONGODB_PORT"))
+	uri, err := GetMongoUri()
+	if err != nil { log.Fatal(err.Error()) }
+
 	//client
-	_, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	// mongodb+srv://<user>:<Password>@<host>:<port>
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	log.Println("Connected to mongoDB")
+
+
+	db_videos := client.Database("videos")
+	db_mp3s := client.Database("mp3s")
+
+	_, err = gridfs.NewBucket(db_videos, options.GridFSBucket().SetName("fs_videos"))
+	if err != nil {
+		log.Fatalf("fs_videos creation failed:\n%s", err.Error())
+	}
+	_, err = gridfs.NewBucket(db_mp3s, options.GridFSBucket().SetName("fs_mp3s"))
+	if err != nil {
+		log.Fatalf("fs_mp3s creation failed:\n%s", err.Error())
+	}
+	log.Println("Created GridFS buckets")
+
 
 }
