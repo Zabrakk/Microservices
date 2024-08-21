451cd74e44f9754eeff709d1b45f9b121df99e83
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-08-21 18:08:03 +0300

    Added comments

--- vid2mp3go/src/converter/main.go
+++ vid2mp3go/src/converter/main.go
@@ -25,22 +25,22 @@ func GetMongoUri() (mongoUri string, err error) {
 
 func main() {
 	log.Println("Converter service starting...")
-	// Connect to MongoDB
 
+	// Connect to MongoDB
 	uri, err := GetMongoUri()
 	if err != nil { log.Fatal(err.Error()) }
 
-	//client
+	// Create MongoDB client / connection
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-	// mongodb+srv://<user>:<Password>@<host>:<port>
 
 	if err != nil { log.Fatal(err.Error()) }
 	log.Println("Connected to mongoDB")
 
-
+	// Create DB handles for the video and mp3 storage databases
 	db_videos := client.Database("videos")
 	db_mp3s := client.Database("mp3s")
 
+	// Create gridFS buckets
 	_, err = gridfs.NewBucket(db_videos, options.GridFSBucket().SetName("fs_videos"))
 	if err != nil {
 		log.Fatalf("fs_videos creation failed:\n%s", err.Error())
@@ -51,16 +51,19 @@ func main() {
 	}
 	log.Println("Created GridFS buckets")
 
+	// Connect to RabbitMQ
 	connection, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672")
 	if err != nil { log.Fatal(err.Error()) }
 	defer connection.Close()
 	log.Println("Connected to RabbitMQ")
 
+	// Open a channel for message receiving
 	channel, err := connection.Channel()
 	if err != nil { log.Fatal(err.Error()) }
 	defer channel.Close()
 	log.Printf("RabbitMQ channel opened")
 
+	// Create a Queue for videos
 	queue, err := channel.QueueDeclare(
 		os.Getenv("VIDEO_QUEUE"),	// Name
 		true,						// Durable
@@ -71,6 +74,7 @@ func main() {
 	)
 	if err != nil { log.Fatal(err.Error()) }
 
+	// Start consuming messages from the queue
 	msgs, err := channel.Consume(
 		queue.Name, // Queue
 		"",     	// Consumer
@@ -84,6 +88,7 @@ func main() {
 
 	var forever chan struct{}
 
+	// Message handler
 	go func() {
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
