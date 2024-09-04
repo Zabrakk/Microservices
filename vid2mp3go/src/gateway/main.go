1c7ca598a75ac63a19c87dc820eb84df3ba5e5f3
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-09-04 19:40:25 +0300

    Added template for each route

--- vid2mp3go/src/gateway/main.go
+++ vid2mp3go/src/gateway/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+var servicePort string = "8080"
+
+func Login(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Register(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Upload(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Download(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	log.Println("Gateway service starting...")
+
+	http.HandleFunc("/login", Login)
+	http.HandleFunc("/register", Register)
+	http.HandleFunc("/upload", Upload)
+	http.HandleFunc("/download", Download)
+
+	log.Println("Gateway service running on port", servicePort)
+	err := http.ListenAndServe(":"+servicePort, nil)
+	if err != nil { log.Fatal(err.Error()) }
+}
