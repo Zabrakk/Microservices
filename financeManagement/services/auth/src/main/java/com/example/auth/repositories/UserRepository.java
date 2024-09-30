048c71f3a87b6097c873599867e840ecbbbcaee6
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-09-30 16:13:28 +0300

    Documentation

--- financeManagement/services/auth/src/main/java/com/example/auth/repositories/UserRepository.java
+++ financeManagement/services/auth/src/main/java/com/example/auth/repositories/UserRepository.java
@@ -6,7 +6,9 @@ import com.example.auth.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-// Represents the data access layer for the User entity
+/**
+ * Represents the data access layer for the User entity
+ */
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 	Optional<User> findByUsername(String username);
